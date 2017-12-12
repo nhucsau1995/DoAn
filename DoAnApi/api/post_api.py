68366c8f4e3ac0fd3f8c81afed8ab4e67ae22765
Phùng Nhục Sầu <nhucsau1995@gmail.com>
2017-12-12 09:01:34 +0700

    đầ

--- DoAnApi/api/post_api.py
+++ DoAnApi/api/post_api.py
@@ -135,25 +135,99 @@ class UserPostDetail(APIView, ApiBase):
         try:
             post = Post.objects.get(slug=slug, user=user)
             post.phone = post.user.phone
+
+            images = Image.objects.filter(post_id=post)
+            post.images = images
             return post
 
         except Post.DoesNotExist:
             raise ApiCustomException(ErrorDefine.POST_NOT_FOUND)
 
+    def get_post(self, user, post):
+        try:
+            try:
+                post.userlikepost_set.get(user_id=user)
+
+                post.is_liked = True
+            except:
+                post.is_liked = False
+
+            post.username = post.user.username
+            post.email = post.user.email
+            post.phone = post.user.phone
+            post.facebook = post.user.facebook
+
+            images = Image.objects.filter(post_id=post)
+            post.images = images
+
+            return post
+
+        except Post.DoesNotExist:
+            raise ApiCustomException(ErrorDefine.POST_NOT_FOUND)
+
+    def update_post(self,slug,user,phone,type,title,video,customer,price,area, province,district, address, description):
+        post = Post.objects.get(slug=slug, user=user)
+        post.phone = phone
+        post.type = type
+        post.title = title
+        post.customer = customer
+        post.price = price
+        post.area = area
+        post.province = province
+        post.district = district
+        post.address = address
+        post.description = description
+        if post.video != video:
+            post.video.delete()
+            post.video = video
+        post.save()
+        return post
+
+    def delete_image(self, id):
+        image = Image.objects.get(id=id)
+        image.image.delete()
+        image.delete()
+
+    def add_image(self, image, post):
+        images = Image(post_id=post, image=image)
+        images.save()
+        return images
+
+
+    def get(self, request, slug, *args, **kwargs):
+        user = self.check_anonymous(request)
+        post = self.get_user_post_detail(user, slug)
+        serializer = PostSerializer(post)
+        return self.as_success(serializer.data)
 
     def put(self, request, slug,*args, **kwargs):
         user = self.check_anonymous(request)
-        post = self.get_user_post_detail(user, slug)
-        serializer = EditPostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return self.as_success(serializer.data)
-        return self.as_errors(serializer.errors)
-
+        phone = request.data['phone']
+        type = request.data['type']
+        title = request.data['title']
+        video = request.data['video']
+        customer = request.data['customer']
+        price = request.data['price']
+        area = request.data['area']
+        province = request.data['province']
+        district = request.data['district']
+        address = request.data['address']
+        description = request.data['description']
+        post = self.update_post(slug,user,phone,type,title,video,customer,price,area, province,district, address, description)
+        for id in request.data['delete']:
+            self.delete_image(id)
+        for image in request.data.getlist('images'):
+            self.create_image(image, post)
+        post = self.get_post(user, post)
+        serializer = PostSerializer(post)
+        return self.as_success(serializer.data)
 
     def delete(self, request, slug ,format=None,*args, **kwargs):
         user = self.check_anonymous(request)
         post = self.get_user_post_detail(user, slug)
+        images = Image.objects.filter(post_id=post)
+        for image in images:
+            image.image.delete()
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -184,9 +258,9 @@ class UserCreatePost(APIView, ApiBase):
         except Post.DoesNotExist:
             raise ApiCustomException(ErrorDefine.POST_NOT_FOUND)
 
-    def create_post(self,user,phone,type,title,video,customer,price,area, province,district, address, decription):
+    def create_post(self,user,phone,type,title,video,customer,price,area, province,district, address, description):
         post = Post(user=user,phone=phone, type=type, title=title,video=video, customer=customer,
-               price=price, area = area, province=province,district=district, address=address, decription=decription)
+               price=price, area = area, province=province,district=district, address=address, description=description)
         post.save()
         return post
 
@@ -209,10 +283,9 @@ class UserCreatePost(APIView, ApiBase):
         province = request.data['province']
         district = request.data['district']
         address = request.data['address']
-        decription = request.data['decription']
-        post = self.create_post(user,phone,type,title,video,customer,price,area, province,district, address, decription)
-        for image in request.data.getlist('image'):
-            print image
+        description = request.data['description']
+        post = self.create_post(user,phone,type,title,video,customer,price,area, province,district, address, description)
+        for image in request.data.getlist('images'):
             self.create_image(image, post)
         post = self.get_post(user, post)
         serializer = PostSerializer(post)
